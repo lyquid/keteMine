d9f5618e3fff199f71c1cbc95ce3dd7b58ab9cad
Alex CB <ketepincho@gmail.com>
2022-11-20 15:09:02 +0100

    Added window size callback

--- src/ketemine.cpp
+++ src/ketemine.cpp
@@ -13,12 +13,17 @@ void errorCallback(int error, const char* description) {
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
-  glViewport(0, 0, width, height);
+  ktp::keteMine::window_size = {width, height};
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void windowSizeCallback(GLFWwindow* window, int width, int height) {
+  ktp::keteMine::window_size = {width, height};
+  // update any perspective matrices used here
 }
 
 void ktp::keteMine::init() {
@@ -31,6 +36,9 @@ void ktp::keteMine::init() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_SAMPLES, 4);
   // window
+  // GLFWmonitor* monitor {glfwGetPrimaryMonitor()};
+  // const GLFWvidmode* video_mode {glfwGetVideoMode(monitor)};
+  // window = glfwCreateWindow(video_mode->width, video_mode->height, "keteMine", monitor, nullptr);
   window = glfwCreateWindow(window_size.x, window_size.y, "keteMine", nullptr, nullptr);
   if (!window) {
     glfwTerminate();
@@ -38,9 +46,11 @@ void ktp::keteMine::init() {
   }
   glfwMakeContextCurrent(window);
   glClearColor(0.f, 0.f, 0.f, 1.f);
+  glViewport(0, 0, window_size.x, window_size.y);
   // callbacks
   glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
   glfwSetKeyCallback(window, keyCallback);
+  glfwSetWindowSizeCallback(window, windowSizeCallback);
   // GLEW
   glewExperimental = GL_TRUE;
   const auto err {glewInit()};
@@ -48,8 +58,6 @@ void ktp::keteMine::init() {
     std::cerr << "GLEW error: " << glewGetErrorString(err) << '\n';
     exit(EXIT_FAILURE);
   }
-
-  glViewport(0, 0, window_size.x, window_size.y);
 
   versionInfo();
 
@@ -59,6 +67,7 @@ void ktp::keteMine::init() {
 void ktp::keteMine::run() {
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0, 0, window_size.x, window_size.y);
 
     gui::layout();
 
