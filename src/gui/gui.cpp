44b5612f49d62d0f338395967503e10b0011ff5c
Alex CB <ketepincho@gmail.com>
2022-11-20 23:55:37 +0100

    Undo previous

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -1,8 +1,8 @@
 #include "gui.hpp"
 
-#include "../lib/imgui/imgui.h"
-#include "../lib/imgui/imgui_impl_glfw.h"
-#include "../lib/imgui/imgui_impl_opengl3.h"
+#include "../../lib/imgui/imgui.h"
+#include "../../lib/imgui/imgui_impl_glfw.h"
+#include "../../lib/imgui/imgui_impl_opengl3.h"
 #include <iostream>
 
 void ktp::gui::init(GLFWwindow* window) {
